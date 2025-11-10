fe38f8cf68b44e805d9fc81ca26e03b3581ec20a
RafaelRodrigues1003 <rafael.rodrigues.rr05@gmail.com>
2025-11-09 23:34:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+//Efeito de deixar o cabeçalho transparente//
+
+window.addEventListener("scroll", function(){
+    let header= document.querySelector('#header')
+    header.classList.toggle('rol', window.scrollY> 0)
+})
+
+const sections = document.querySelectorAll('section');
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            }
+        });
+    },
+    { threshold: 0.5 }
+);
+
+//Efeito de escrever na página HOME//
+
+sections.forEach(section => observer.observe(section));
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Cultura", "Estudo", "Desenvolvimento", "Sustentabilidade"];
+const typingDelay = 150;
+const erasingDelay = 85;
+const newTextDelay = 1800;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { 
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+            function toggleMenu() {
+                document.querySelector("nav ul").classList.toggle("open")}
